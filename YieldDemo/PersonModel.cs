26f63b3d5c88978f6fd55f5d1a2035303376e090
Adithya Vijay K <adithyavj@gmail.com>
2021-07-26 19:45:08 +0530

    Add sample program with generic code

---
+++ YieldDemo/PersonModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YieldDemo
+{
+    public class PersonModel
+    {
+        public PersonModel(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Console.WriteLine($"Initialized user {FirstName} {LastName}");
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- YieldDemo/Program.cs
+++ YieldDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace YieldDemo
 {
@@ -6,7 +7,31 @@ namespace YieldDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Start of the App");
+
+            var people = DataAccess.GetPeople();
+            foreach (var p in people)
+            {
+                Console.WriteLine($"Read {p.FirstName} {p.LastName}");
+            }
+
+            Console.WriteLine("End of the App");
+        }
+    }
+
+
+
+    public static class DataAccess
+    {
+        public static IEnumerable<PersonModel> GetPeople()
+        {
+            List<PersonModel> output = new();
+
+            output.Add(new PersonModel("Adithya", "Vijay"));
+            output.Add(new PersonModel("Arjun", "Narayanan"));
+            output.Add(new PersonModel("Jetlin", "Yeldo"));
+
+            return output;
         }
     }
 }
